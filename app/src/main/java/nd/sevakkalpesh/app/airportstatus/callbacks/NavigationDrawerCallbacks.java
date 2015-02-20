662b26f70c7335ab434e91469a80235c92b5b2e4
kalpesh <sevakkalpesh@gmail.com>
2015-02-20 12:21:58 +0000

    Initial commit

---
+++ app/src/main/java/nd/sevakkalpesh/app/airportstatus/callbacks/NavigationDrawerCallbacks.java
@@ -0,0 +1,8 @@
+package nd.sevakkalpesh.app.airportstatus.callbacks;
+
+/**
+ * Created by Kalpesh on 18/2/2015.
+ */
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
---
+++ app/src/main/java/nd/sevakkalpesh/app/airportstatus/model/NavigationItem.java
@@ -0,0 +1,32 @@
+package nd.sevakkalpesh.app.airportstatus.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Kalpesh on 18/2/2015.
+ */
+public class NavigationItem {
+    private String mText;
+    private Drawable mDrawable;
+
+    public NavigationItem(String text, Drawable drawable) {
+        mText = text;
+        mDrawable = drawable;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public Drawable getDrawable() {
+        return mDrawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        mDrawable = drawable;
+    }
+}
